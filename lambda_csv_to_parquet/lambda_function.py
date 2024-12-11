ed47eeec653043f4b618267cafe12ad2137eabf6
lchen <73617864+lchen-2101@users.noreply.github.com>
2024-12-11 15:34:36 -0800

    raise on report.csv to stop downstream destination lambda from processing

--- lambda_csv_to_parquet/lambda_function.py
+++ lambda_csv_to_parquet/lambda_function.py
@@ -22,35 +22,38 @@ def lambda_handler(event, context):
     fname = paths[-1]
     fprefix = '.'.join(fname.split('.')[:-1])
     res_folder = f"{'/'.join(paths[:-1])}/{fprefix}_pqs/"
-    try:
-        pq_idx = 1
-        response = s3.get_object(Bucket=bucket, Key=key)
-        batch_size = int(os.getenv('BATCH_SIZE', 50000))
-        log.info(f"batch size: {batch_size}")
-        
-        for chunk in pa.read_csv(response['Body'], dtype=str, keep_default_na=False, chunksize=batch_size):
-            buffer = io.BytesIO()
-            chunk.to_parquet(buffer)
-            buffer.seek(0)
-            s3.upload_fileobj(buffer, bucket, f"{res_folder}{pq_idx:05}.parquet")
-            pq_idx += 1
+    if "report.csv" not in fname:
+        try:
+            pq_idx = 1
+            response = s3.get_object(Bucket=bucket, Key=key)
+            batch_size = int(os.getenv('BATCH_SIZE', 50000))
+            log.info(f"batch size: {batch_size}")
+            
+            for chunk in pa.read_csv(response['Body'], dtype=str, keep_default_na=False, chunksize=batch_size):
+                buffer = io.BytesIO()
+                chunk.to_parquet(buffer)
+                buffer.seek(0)
+                s3.upload_fileobj(buffer, bucket, f"{res_folder}{pq_idx:05}.parquet")
+                pq_idx += 1
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps('done converting!'),
-            "Records": [
-                {
-                    "s3": {
-                        "bucket": {
-                            "name": bucket
-                        },
-                        "object": {
-                            "key": res_folder
+            return {
+                'statusCode': 200,
+                'body': json.dumps('done converting!'),
+                "Records": [
+                    {
+                        "s3": {
+                            "bucket": {
+                                "name": bucket
+                            },
+                            "object": {
+                                "key": res_folder
+                            }
                         }
                     }
-                }
-            ]
-        }
-    except Exception as e:
-        log.exception('Failed to process {} in {}'.format(key, bucket))
-        raise e
+                ]
+            }
+        except Exception as e:
+            log.exception('Failed to process {} in {}'.format(key, bucket))
+            raise e
+    else:
+        raise RuntimeWarning("not processing report.csv: %s", fname)
