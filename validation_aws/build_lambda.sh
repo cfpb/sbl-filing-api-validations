866193674a46f3ebbc9b24e25d23acdcbdd93993
lchen <73617864+lchen-2101@users.noreply.github.com>
2024-12-27 09:41:20 -0800

    clean script

--- validation_aws/build_lambda.sh
+++ validation_aws/build_lambda.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 while getopts p:t: option; do
-    case "${option}" in
-        p) path="${OPTARG}" ;;
-        t) tag="${OPTARG}" ;;
-        *) echo "Usage: $0 -p <lambda_path> -t <tag>" >&2
+    case $option in
+        p) path=$OPTARG ;;
+        t) tag=$OPTARG ;;
+        *) echo "Usage: $0 -p <lambda_path> -t <image_tag>" >&2
            exit 1 ;;
     esac
 done
-if [[ -z "${path}" ||  -z "${tag}" ]]; then
+if [[ -z $path ||  -z $tag ]]; then
     echo "Usage: $0 -p <lambda_path> -t <tag>" >&2
     exit 1
 fi
