9e367e61ef340a6cb8f906d8d0994016f3fa7a8d
Adam <41971533+jcadam14@users.noreply.github.com>
2025-01-20 09:19:33 -0700

    Added linting and pytests

---
+++ do_sqs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+run_in_docker="$1"
+echo $run_in_docker
+if [ ! -z "$run_in_docker" ]
+then
+    docker kill local_validation_run
+    docker rm local_validation_run
+    docker build --platform linux/amd64 -t local_validation_run:latest -f Local_Dockerfile .
+    docker run -d --platform linux/amd64 --network sbl-project_default -v /tmp/filing_bucket/upload/:/tmp/filing_bucket/upload/ -e ENV=LOCAL -e DB_NAME=filing -e DB_USER=filing_user -e DB_PWD=filing_user -e DB_HOST=pg --name local_validation_run local_validation_run:latest
+    docker container ls
+else
+    docker build --platform linux/amd64 -t sqs-parquet:latest -f SQS_Dockerfile --build-arg SQS_PATH=src/sbl_validation_processor/sqs_csv_to_parquet .
+    docker build --platform linux/amd64 -t sqs-validate:latest -f SQS_Dockerfile --build-arg SQS_PATH=src/sbl_validation_processor/sqs_parquet_validation .
+    docker build --platform linux/amd64 -t sqs-aggregator:latest -f SQS_Dockerfile --build-arg SQS_PATH=src/sbl_validation_processor/sqs_validation_aggregator .
+    docker build --platform linux/amd64 -t sqs-parquet-job:latest -f Job_Dockerfile --build-arg JOB_PATH=src/sbl_validation_processor/sqs_csv_to_parquet .
+    docker build --platform linux/amd64 -t sqs-validator-job:latest -f Job_Dockerfile --build-arg JOB_PATH=src/sbl_validation_processor/sqs_parquet_validation .
+    docker build --platform linux/amd64 -t sqs-aggregator-job:latest -f Job_Dockerfile --build-arg JOB_PATH=src/sbl_validation_processor/sqs_validation_aggregator .
+    docker tag sqs-parquet:latest 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-parquet:latest.2
+    docker tag sqs-parquet-job:latest 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-parquet-job:latest.2
+    docker tag sqs-validate:latest 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-validate:latest.2
+    docker tag sqs-validator-job:latest 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-validator-job:latest.2
+    docker tag sqs-aggregator:latest 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-aggregator:latest.2
+    docker tag sqs-aggregator-job:latest 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-aggregator-job:latest.2
+    docker push 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-parquet:latest.2
+    docker push 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-parquet-job:latest.2
+    docker push 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-validate:latest.2
+    docker push 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-validator-job:latest.2
+    docker push 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-aggregator:latest.2
+    docker push 099248080076.dkr.ecr.us-east-1.amazonaws.com/cfpb/regtech/sqs-aggregator-job:latest.2
+    kubectl rollout restart deployment -n regtech sqs-csv-poller
+    kubectl rollout restart deployment -n regtech sqs-pqs-poller
+    kubectl rollout restart deployment -n regtech sqs-res-poller
+fi
+
